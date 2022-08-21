79d8192799ca032969b656b3888b1468da9fe9d2
JimChu <40918695+vga53327@users.noreply.github.com>
2022-08-21 20:20:53 +0800

    WeHelpPage2
    WeHelpPage2

---
+++ assets/js/WeHelp.js
@@ -0,0 +1,9 @@
+
+// let list = document.getElementById("list")
+// list.innerHTML="Loading"
+// 透過 AJAX fetch API 連線到遠端的網址，抓取網址
+/*
+  fetch(URL).then(function(response){
+    // response 物件，代表伺服器的回應
+  })
+*/
